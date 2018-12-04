7f6e117a2d7ead0ea4b134e2072ef7f56032699b
Zalkar <zalkar@bennington.edu>
2018-12-03 22:43:24 -0400

    Signin/Register set up and working

--- app.py
+++ app.py
@@ -18,9 +18,24 @@ app = Flask(__name__)
 engine = create_engine('postgres://evvuybdksjmxgc:793e1cae388415ba1126d5407211d96e624c402990c436b69b0c562fc91ceed5@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dabk5r98m602n0')
 db = scoped_session(sessionmaker(bind=engine))
 
+
 # Login
 @app.route("/login", methods=['POST'])
 def login():
+    if request.method == 'POST':
+        result = db.execute("SELECT * FROM users WHERE name = :name AND password = :password",
+                   {"name": request.form['name'], "password": request.form['password']}).fetchone()
+        db.commit()
+
+    if result is None:
+        return "Failure"
+
+    return "Success"
+
+
+# Register
+@app.route("/register", methods=['POST'])
+def register():
     if request.method == 'POST':
         db.execute("INSERT INTO users (name, password) VALUES (:name, :password)",
                    {"name": request.form['name'], "password": request.form['password']})
