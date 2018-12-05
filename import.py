c6f6ba64b0368f64d079023409284cab438375b9
Zalkar <zalkar@bennington.edu>
2018-12-05 18:27:47 -0400

    Import file added. books.csv translated to the database.

---
+++ import.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#
+# import.py - imports books.csv to the database
+#
+# Author: Zalkar Ziiaidin uulu
+# Date created: 12/5/2018
+#
+#
+
+# import files
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import csv
+
+# basic setup
+app = Flask(__name__)
+
+engine = create_engine('postgres://evvuybdksjmxgc:793e1cae388415ba1126d5407211d96e624c402990c436b69b0c562fc91ceed5@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dabk5r98m602n0')
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def main():
+    file = open("books.csv")
+    reader = csv.reader(file)
+
+    for id, title, author, year in reader:
+        db.execute("INSERT INTO books (id, title, author, year) VALUES (:id, :title, :author, :year)",
+                   {"id": id, "title": title, "author": author, "year": year})
+        print({id}, " ", {title})
+        db.commit()
+
+
+if __name__ == "__main__":
+    main()
